2a9ef4b1e56c1231c3cb39cc6dc9893a1790ed05
Ben Johnson <holloo111@gmail.com>
2018-03-19 11:32:13 -0600

    yes

--- src/Bens_Files/Ben.js
+++ src/Bens_Files/Ben.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Ben() {
     return (
          <div>
-            <h1>Ben is the best</h1>
+            <h1>Ben is the most amazing ever</h1>
         </div>
     )
 }
