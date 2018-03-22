2f3d0848e2ac091c148a175f9bc32b23b053d6ce
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-22 11:59:29 -0600

    StoryEditorModal complete. Is not functional.

--- src/Components/StoryEditorModal/StoryEditorModal.js
+++ src/Components/StoryEditorModal/StoryEditorModal.js
@@ -4,18 +4,26 @@ import image from '../../Assets/FamilySearchTutorial.png'
 
 class StoryEditorModal extends Component {
 
-  handleClick(type){
-    if (type === 'backdrop'){
-      this.props.close()
-    }
-  }
+  // handleClick(type){
+  //   if (type === 'backdrop'){
+  //     this.props.close()
+  //   }
+  // }
 
   render(){
     return(
-      <div onClick = {()=>this.handleClick('backdrop')} className = 'transparent-background' name = 'backdrop'>
-          <div onClick = {()=>this.handleClick('modal')} className = 'modal-style'>
-            <img src = {image} className = 'fs-tutorial'></img>
+      <div //onClick = {()=>this.handleClick('backdrop')} 
+        className = 'transparent-background' name = 'backdrop'>
+        <div className = 'mid-modal'>
+          <div 
+          //onClick = {()=>this.handleClick('modal')} 
+            className = 'modal-style'>
+            <input placeholder = 'FamilySearch ID'/>
+            <button> Import from Family Search </button>
+            <a href = {image}><img src = {image} className = 'fs-tutorial'></img></a>
+            <p> To import from Family Search, login to your Family Search account and navigate to your family tree. Find the person you would like to import stories, pictures or audio from. You will then find a seven digit number. Take the seven digit number and enter it into the field above.</p>
           </div>
+        </div>
       </div>
     )
   }
