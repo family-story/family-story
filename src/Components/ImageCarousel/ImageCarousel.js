48687ff8a661660763c21105b3bc3a02b74d9271
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-26 14:25:11 -0600

    storyViewModal prepped to wire with store

--- src/Components/ImageCarousel/ImageCarousel.js
+++ src/Components/ImageCarousel/ImageCarousel.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react'
 import Slider from 'react-slick'
 
+const data = require('../../Views/StoryViewModal/dummy')
+
 class ImageCarousel extends Component {
   constructor(props){
     super(props);
   
     this.state = {
-        pics: []
+        pics: [
+            {
+                "media_id": 1,
+                "media_type": "pic",
+                "media_ref": "https://www.familysearch.org/dzpatron/v1/TH-301-41540-135-27/scale?width=800&ctx=ArtCtxPublic&angle=0"
+            },
+            {
+                "media_id": 2,
+                "media_type": "pic",
+                "media_ref": "https://www.familysearch.org/dzpatron/v1/TH-303-45428-574-53/scale?width=800&ctx=ArtCtxPublic&angle=0"
+            },
+            {
+                "media_id": 3,
+                "media_type": "pic",
+                "media_ref": "https://www.familysearch.org/dzpatron/v1/TH-300-45428-733-1/scale?width=800&ctx=ArtCtxPublic&angle=0"
+            },
+            {
+                "media_id": 4,
+                "media_type": "pic",
+                "media_ref": "https://www.familysearch.org/dzpatron/v1/TH-300-48239-1135-19/scale?width=800&ctx=ArtCtxPublic&angle=0"
+            }
+        ]
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.pics){
+        this.setState({
+            pics: nextProps.pics
+        })
     }
   }
 
   render(){
+      let eventPics = this.state.pics.map(picture=>{
+          
+          return (
+              <div className = 'carousel-image-container' key = {picture.media_id}>
+                <img src = {picture.media_ref} alt =''/>
+              </div>
+          )
+      })
+
     let settings = {
         dots: true,
         arrows: true,
@@ -20,7 +60,7 @@ class ImageCarousel extends Component {
     return(
       <div className = 'carousel-container'>
         <Slider {...settings}>
-            {this.props.pics}
+            {eventPics}
         </Slider>
       </div>
     )
