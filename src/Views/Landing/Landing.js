4856721027c8932011df197e48c4d4112d836a3d
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-27 18:21:16 -0600

    Landing

--- src/Views/Landing/Landing.js
+++ src/Views/Landing/Landing.js
@@ -10,10 +10,10 @@ export default function Landing(props){
         <div className = 'landing-mid'>
           <span className = 'landing-welcome-1'>
             Welcome to Family Story
+          </span>
             <div className = 'landing-welcome-2'>
               where your stories come to life...
             </div>
-          </span>
         </div> 
       </div> 
       <a href={process.env.REACT_APP_LOGIN} ><button>LOGIN</button></a>
