995f3967faf56a64c540cc161bb7c76a3948739c
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-29 17:44:45 -0600

    StoryView styling done

--- src/Views/StoryView/StoryView.js
+++ src/Views/StoryView/StoryView.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import MapDisplay from '../../Components/MapDisplay/MapDisplay';
 import ImageCarousel from '../../Components/ImageCarousel/ImageCarousel';
 import AudioPlayer from './AudioPlayer';
+import NavBar from '../../Components/NavBar/NavBar'
 
 
 class StoryView extends Component {
@@ -30,45 +31,39 @@ handlePreviousButton(){
 }
 
   render(){
-    // let sortedEvents = this.state.story[0].events.sort((a, b)=>a.event_id - b.event_id)
-    // let pics = sortedEvents[this.state.event].media.filter(media => media.media_type === 'pic').map(mediaObj => {
-    //     return (
-    //       <div>
-    //           <img src = {mediaObj.media_ref} alt={mediaObj.media_id}/>
-    //       </div>
-    //   )
-    // })
     
     let audioTags = null;
 
     if (this.props.story[0].events[this.state.eventIndex].media.filter(obj => obj.media_type === "audio").length > 0) {
-      audioTags = this.props.story[0].events[this.state.eventIndex].media.filter(obj => obj.media_type === "audio").map((obj, i) => <AudioPlayer audio = {obj} key = {i}/>)
+      audioTags = this.props.story[0].events[this.state.eventIndex].media.filter(obj => obj.media_type === "audio").map((obj, i) => <div className = 'story-audio'><AudioPlayer audio = {obj} key = {i}/></div>)
     }
     return(
-      
-      <div className = 'transparent-background'>
-        <div className = 'mid-modal'>
-        <div className = 'story-view-modal'>
-        <h1>{this.props.story[0].story_title}</h1>
+      <div className = 'story-view-container'>
+        <div className = 'story-view-title'>
+          <h1>{this.props.story[0].story_title}</h1>
+        </div>
+        <div className = 'divider-1'></div>
         <section className = 'eventViewer'>
-          <MapDisplay location={this.props.story[0].events[this.state.eventIndex].location}/>
-          <aside>
-            <h3>{this.props.story[0].events[this.state.eventIndex].event_title}</h3>
-            <p>{this.props.story[0].events[this.state.eventIndex].event_txt}</p>
+          <div className = 'story-left'>
+            <MapDisplay location={this.props.story[0].events[this.state.eventIndex].location}/>
+            <h3 className = 'location'>{this.props.story[0].events[this.state.eventIndex].location}</h3>
+            <div className = 'carousel'>
+              <ImageCarousel pics = {this.props.story[0].events[this.state.eventIndex].media.filter(obj => obj.media_type === "pic")}/>
+            </div>
+            {audioTags} 
+          </div>
+          <aside className = 'story-right'>
+            <h3 className = 'event-title'>{this.props.story[0].events[this.state.eventIndex].event_title}</h3>
+            <span className = 'event-date'>{this.props.story[0].events[this.state.eventIndex].date}</span>
+            <p className = 'event-desc'>{this.props.story[0].events[this.state.eventIndex].event_txt}</p>
           </aside>
-          <ImageCarousel pics = {this.props.story[0].events[this.state.eventIndex].media.filter(obj => obj.media_type === "pic")}/>
-          {audioTags}
         </section>
-          
-        <button disabled={this.state.eventIndex===0} onClick = {()=>this.handlePreviousButton()}> Prev </button>
-
-        <button disabled={this.state.eventIndex===(this.props.story[0].events.length - 1)} id='next' onClick = {()=>this.handleNextButton()}> Next </button>
-        
-        <Link to="/home"><button>Back To Your Stories</button></Link>
-
-        
+        <div className = 'story-buttons'>
+          <Link to="/home"><button className = 'story-button-back'>Back to stories</button></Link>
+          <button className = 'story-button' disabled={this.state.eventIndex===0} onClick = {()=>this.handlePreviousButton()}> Prev </button>
+          <button className = 'story-button' disabled={this.state.eventIndex===(this.props.story[0].events.length - 1)} id='next' onClick = {()=>this.handleNextButton()}> Next </button>
         </div>
-        </div>
+      
       </div>
     )
   }
